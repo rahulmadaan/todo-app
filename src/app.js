c1c7562ad68ac94c7dcaf82c0909f26a121aad8f
Deepika <44020585+deepikabartwal@users.noreply.github.com>
2019-01-24 18:21:26 +0530

    added few function to take user account details
    added function to take read and validate user details right now for
    single dummy user

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
 const { readFile } = require('fs');
 const RequestHandler = require('./frameWork.js');
 const app = new RequestHandler();
-
 const logRequests = function(req, res, next) {
 	console.log(req.url);
 	console.log(req.method);
 	next();
+};
+
+const readBody = function(req, res, next) {
+	let text = '';
+	req.on('data', chunk => {
+		text = text + chunk;
+	});
+	req.on('end', () => {
+		req.body = text;
+		next();
+	});
 };
 
 const addPathPrefix = url => `./public${url}`;
@@ -18,7 +28,7 @@ const getPath = function(url) {
 	return path;
 };
 
-const send = function(res, statusCode, content) {
+const send = function(res, content, statusCode = 200) {
 	res.statusCode = statusCode;
 	res.write(content);
 	res.end();
@@ -26,14 +36,14 @@ const send = function(res, statusCode, content) {
 };
 
 const sendNotFound = function(res) {
-	return send(res, 404, 'Page Not Found');
+	return send(res, 'Page Not Found', 404);
 };
 
 const serveFile = function(req, res) {
 	let path = getPath(req.url);
 	readFile(path, (err, content) => {
 		if (!err) {
-			send(res, 200, content);
+			send(res, content);
 			return;
 		}
 		sendNotFound(res);
@@ -43,11 +53,43 @@ const serveFile = function(req, res) {
 
 const render404Page = function(req, res) {
 	fs.readFile('./public/notFound.html', (error, content) => {
-		send(res, 404, content);
+		send(res, content, 404);
 	});
 };
 
-app.use(logRequests);
-app.use(serveFile);
+const parseUserDetails = function(userDetails) {
+	const args = {};
+	const splittedDetails = userDetails.split('&');
+	const mappedDetails = splittedDetails.map(details => details.split('='));
+	mappedDetails.map(key => {
+		args[key[0]] = key[1];
+	});
+	return args;
+};
 
+const isDetailsMatching = function(userDetails) {
+	const userName = userDetails.username;
+	const password = userDetails.password;
+	return (userName == 'rahul') & (password == 1234);
+};
+
+const validateUser = function(req, res, next) {
+	const userDetails = parseUserDetails(req.body);
+	if (isDetailsMatching(userDetails)) {
+		next();
+		return;
+	}
+	res.setHeader('Location', '/login.html');
+	send(res, 'Wrong user name and password', 302);
+};
+
+const viewList = function(req, res, next) {
+	send(res, 'you are on view list page');
+	next();
+};
+
+app.use(readBody);
+app.use(logRequests);
+app.post('/dashboard.html', validateUser);
+app.use(serveFile);
 module.exports = app.handleRequest.bind(app);
