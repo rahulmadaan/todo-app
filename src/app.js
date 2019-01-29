a16b098280db1d70b321087bb52a4a22b5fe0374
Deepika <44020585+deepikabartwal@users.noreply.github.com>
2019-01-29 19:18:04 +0530

    Added handlers for funcitons like addTask, deleteList etc.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,78 @@
 const { readFile, writeFile } = require('fs');
 const RequestHandler = require('./frameWork.js');
+const TASKS_DETAILS_FILE = './data/taskDetails.json';
 
 const app = new RequestHandler();
 const logRequests = function(req, res, next) {
 	console.log(req.url);
 	console.log(req.method);
 	next();
+};
+
+const template = {
+	viewTask: `<html>
+	<head>
+		<title>Mingle-ViewList</title>
+		<link rel="stylesheet" href="main.css" />
+		<script src="viewTasks.js"></script>
+	</head>
+	<body>
+		<main>
+			<div class="viewListHeader" xz>
+				<header>
+					<h1><u>Your listName List</u></h1>
+					</header>
+					<hr />
+					</div>
+					<div class="toDoLists">
+					<table id="userLists">
+					<thead>
+					<td><strong>List Items</strong><button onclick=confirmDeletion() >&#x1F5D1</button></td>
+						</thead>						
+						</table>
+						<button type="submit" value='addTask' id="addNewTaskButton" onclick = addTaskInterface()> Create new Task</button>
+						<div id="addNewTask"></div>
+			</div>
+		</main>
+	</body>
+</html>
+`,
+	newTaskForm: `<form
+action="/addNewTask"
+method="POST"
+style="width:680px;
+margin: 0 auto;
+text-align:center;"
+>
+<lable>Task Description:</lable> <br />
+<input
+	type="text"
+	name="taskDescription"
+	placeholder="Enter Task description"
+	style="width:240px; height:30px;font-size: 20px;border-radius:7.25px"
+/>
+<input
+	type="submit"
+	value="Save Task"
+	
+	style="width:150px; height:30px;font-size: 20px;border-radius: 7.25px"
+/>
+</form>`,
+
+	confirmDeletion: listId => `<form
+	action=/deleteList?listId=${listId};
+method="POST"
+style="width:680px;
+margin: 0 auto;
+text-align:center;"
+>
+<lable>Are you sure to Delete this list?</lable> <br />
+<input
+	type="submit"
+	value="Delete"
+	style="width:150px; height:30px;font-size: 20px;border-radius: 7.25px"
+/>
+</form>`
 };
 
 const readBody = function(req, res, next) {
@@ -72,11 +139,14 @@ const isDetailsMatching = function(userDetails) {
 const validateUser = function(req, res, next) {
 	const userDetails = parseUserDetails(req.body);
 	if (isDetailsMatching(userDetails)) {
-		res.setHeader('Set-Cookie', 'userId=1548394081634');
+		res.writeHead(302, {
+			Location: '/Dashboard.html',
+			'Set-Cookie': 'userId=1548394081634'
+		});
 		next();
 		return;
 	}
-	res.setHeader('Location', '/login.html');
+	res.setHeader('Location', '/');
 	send(res, 'Wrong user name and password', 302);
 };
 
@@ -117,39 +187,174 @@ const parseListDetails = function(listDetails) {
 	return args;
 };
 
-const addIdentity = function(listToIdentify) {
+const addIdentity = function(listToIdentify, suffix = 'l') {
 	const prefix = 'm_r-';
 	const numeric = Date.now();
-	const postfix = '-l';
-	listToIdentify.listId = prefix + numeric + postfix;
+	const postfix = '-' + suffix.slice(0, 1);
+	listToIdentify[suffix + 'Id'] = prefix + numeric + postfix;
 	return listToIdentify;
 };
+const getList = function(listItem) {
+	const parsedListItem = parseListDetails(listItem);
+	const enhancedListItem = addIdentity(parsedListItem, 'list');
+	return enhancedListItem;
+};
+
+const createInstanceInTaskDetails = function(userId, listId) {
+	readFile(TASKS_DETAILS_FILE, 'utf8', (err, contents) => {
+		const newInstance = { userId: userId, listId: listId, task: [] };
+		const parsedList = JSON.parse(contents);
+		parsedList.push(newInstance);
+		writeFile('./data/taskDetails.json', JSON.stringify(parsedList), err => {});
+	});
+};
+
 const addNewList = function(req, res, next) {
 	const userId = getUserIdByCookie(req.headers.cookie);
-	const newListItem = parseListDetails(req.body);
-	const enhancedListItem = addIdentity(newListItem);
+	const list = getList(req.body);
+	const listId = list.listId;
 	readFile('./data/listsDetails.json', (err, lists) => {
 		const ourCopy = JSON.parse(lists);
-		ourCopy[userId].push(enhancedListItem);
+		ourCopy[userId].push(list);
 		writeFile('./data/listsDetails.json', JSON.stringify(ourCopy), err => {});
+		createInstanceInTaskDetails(userId, listId);
 	});
 	next();
 };
 
+const extractListId = function(url) {
+	return url.split('=')[1];
+};
+
+const filterRequiredList = function(
+	allTaskLists,
+	requiredUserid,
+	requiredListId
+) {
+	const lists = JSON.parse(allTaskLists);
+	const requiredList = lists.filter(list => {
+		if (list.userId == requiredUserid && list.listId == requiredListId) {
+			return list;
+		}
+	});
+	return requiredList[0];
+};
+const giveStatus = function(statusBoolean) {
+	if (statusBoolean === 0) {
+		return 'To-Do';
+	}
+	return 'Done';
+};
+
+const createTaskRow = function(description, statusBoolean) {
+	let status = giveStatus(statusBoolean);
+	return `<tr><td >${description}</td><td>${status}</td></tr>`;
+};
+
+const parseTasks = function(requiredList) {
+	let html = '<table class = "taskList">';
+	listDetails = requiredList.map(list => {
+		html += createTaskRow(list.itemDescription, list.status);
+	});
+
+	return html;
+};
+
 const getTasks = function(req, res) {
-	res.write('');
+	const listId = extractListId(req.url);
+	const userId = getUserIdByCookie(req.headers.cookie);
+	readFile('./data/taskDetails.json', 'utf8', (err, contents) => {
+		const requiredTaskList = filterRequiredList(contents, userId, listId);
+		const html = parseTasks(requiredTaskList.task);
+		const form = template.viewTask;
+		send(res, form + html);
+	});
+};
+
+const renderNewTaskForm = function(req, res, next) {
+	const form = template.newTaskForm;
+	send(res, form);
+};
+
+const renderConfirmDeletionForm = function(req, res) {
+	const listId = extractListId(req.headers.referer);
+	const form = template.confirmDeletion(listId);
+	send(res, form);
+};
+
+const updateTaskList = function(userId, listId, task) {
+	readFile(TASKS_DETAILS_FILE, 'utf8', (err, content) => {
+		const parsedTaskDetails = JSON.parse(content);
+		const filteredTaskDetails = parsedTaskDetails.filter(
+			taskList => taskList.listId == listId
+		);
+		const newTaskItem = { itemDescription: task, status: 0 };
+		const newTask = addIdentity(newTaskItem, 'task');
+		filteredTaskDetails[0].task.push(newTask);
+		writeFile(
+			'./data/taskDetails.json',
+			JSON.stringify(parsedTaskDetails),
+			err => {}
+		);
+	});
+};
+
+const addTaskInList = function(req, res, next) {
+	const task = req.body.split('=')[1];
+	const userId = getUserIdByCookie(req.headers.cookie);
+	const listId = extractListId(req.headers.referer);
+	updateTaskList(userId, listId, task);
+	res.writeHead(302, {
+		location: req.headers.referer
+	});
+	res.end();
+};
+
+const updateListTasks = function(userId, listId) {
+	readFile(TASKS_DETAILS_FILE, 'utf8', (err, contents) => {
+		const parsedList = JSON.parse(contents);
+		const remainingTasks = parsedList.filter(
+			list => list['listId'] != listId && userId['userId'] != userId
+		);
+		writeFile(
+			'./data/taskDetails.json',
+			JSON.stringify(remainingTasks),
+			err => {}
+		);
+	});
+};
+
+const deleteList = function(req, res) {
+	const userId = getUserIdByCookie(req.headers.cookie);
+	const listId = extractListId(req.headers.referer);
+	readFile('./data/listsDetails.json', (err, lists) => {
+		const parsedList = JSON.parse(lists);
+		const usersToDos = parsedList[userId];
+		const remainingToDos = usersToDos.filter(list => list['listId'] != listId);
+		parsedList[userId] = remainingToDos;
+		writeFile(
+			'./data/listsDetails.json',
+			JSON.stringify(parsedList),
+			err => {}
+		);
+		updateListTasks(userId, listId);
+	});
+	res.writeHead(302, {
+		location: '/dashboard.html'
+	});
 	res.end();
 };
 
 app.use(readBody);
 app.use(logRequests);
-
+app.post('/login', validateUser);
 app.post('/dashboard.html', addNewList);
-
 app.get('/viewList', viewList);
-// app.post('/dashboard.html', validateUser);
-
 app.get(/\/viewTasks.html\?listId=/, getTasks);
+app.get('/newTaskForm', renderNewTaskForm);
+app.post('/addNewTask', addTaskInList);
+app.get('/confirmDeletion', renderConfirmDeletionForm);
+app.post(/\/deleteList\?listId=/, deleteList);
 
 app.use(serveFile);
 module.exports = app.handleRequest.bind(app);
