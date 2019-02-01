9626da5f9282bd52fe1be1a4471925219d598e40
Deepika <44020585+deepikabartwal@users.noreply.github.com>
2019-02-01 10:18:03 +0530

    tests for task model

---
+++ test/taskTest.js
@@ -0,0 +1,17 @@
+const { Task } = require('../src/task.js');
+const assert = require('assert');
+describe('task', () => {
+	const toDoTask = new Task('wake up at 5 am', 0, 1234);
+	describe('editing task description', () => {
+		it('should edit task description', () => {
+			toDoTask.editDescription('wake up at 5:30am');
+			assert.equal(toDoTask.getDescription(), 'wake up at 5:30am');
+		});
+	});
+	describe('updating status of task', () => {
+		it('should change status of task', () => {
+			toDoTask.toggleStatus();
+			assert.equal(toDoTask.getStatus(), 1);
+		});
+	});
+});
